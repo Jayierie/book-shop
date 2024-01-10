d0b516d85b03ac127e25283bc4de8733a7a2a4ab
Jere <noeh3914@gmail.com>
2024-01-10 14:03:23 +0630

    card add

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -2,11 +2,13 @@ package com.example.bookshop.controller;
 
 import com.example.bookshop.entity.BookId;
 import com.example.bookshop.service.BookService;
+import com.example.bookshop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,12 +21,17 @@ import java.util.Objects;
 public class BookController {
 
     private final BookService bookService;
+    private final CartService cartService;
 
     @GetMapping("/list-books")
     public String listBooks(Model model){
         model.addAttribute("books",
                 bookService.listBooks());
         return "listbooks";
+    }
+    @ModelAttribute("cartSize")
+    public Integer cartSize() {
+        return cartService.cartSize();
     }
 
     //book/book-details?id=1&isbn=ISBN-1234
---
+++ src/main/java/com/example/bookshop/service/CartBean.java
@@ -0,0 +1,25 @@
+package com.example.bookshop.service;
+
+import com.example.bookshop.dta.CartItem;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+@SessionScope
+public class CartBean {
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    public void addCartItem(CartItem cartItem) {
+        cartItems.add(cartItem);
+    }
+
+    public Integer cartSize() {
+        return cartItems.size();
+    }
+
+}
